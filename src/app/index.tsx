f24d7101fbc3a1acd348e6234a25e6ba8cce72ac
Christoph Wiese <32820890+WieseChristoph@users.noreply.github.com>
2023-06-17 00:46:51 +0200

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack } from "expo-router";
+
+const Index = () => {
+	return (
+		<SafeAreaView className="bg-[#1F104A]">
+			{/* Changes page title visible on the header */}
+			<Stack.Screen options={{ title: "Home Page" }} />
+			<View className="h-full w-full p-4">
+				<Text className="mx-auto pb-2 text-4xl font-bold text-white">
+					<Text className="text-pink-400">Game</Text>server Status
+				</Text>
+
+				<Button title="Add Server" color={"#f472b6"} />
+			</View>
+		</SafeAreaView>
+	);
+};
+
+export default Index;
